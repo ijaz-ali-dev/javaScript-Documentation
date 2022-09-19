6afa5b3f30c237f82ae98affb0775332d058483e
Ijaz Ali <62906306+ijaz-ali-dev@users.noreply.github.com>
2022-09-19 08:59:58 -0700

    `Rule of variables`

---
+++ Basic Programming/3.js
@@ -0,0 +1,21 @@
+// rule for the naming the variables
+// 1 - you cannot start with numbers
+// var 1name = "foo"; invalid name1 
+
+var name1 = "foo";  
+console.log(name1);
+var name2 = 10;
+console.log(name2);
+console.log(name2 + 12);
+console.log(name2 * 20);
+console.log(name2 / 2);
+console.log(name2 ** 20); 
+console.log(name2 ** 0.5); //squre root
+
+// you can used only the underscore and dollar symbol
+var first_name = "foo"; //snake case writing
+console.log(first_name);
+var firstName = "bar"; //camel case writing
+console.log(firstName);
+// start with the small letter and use camel case
+
