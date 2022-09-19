8ce4ba6b6c3050e879960c30f7de89a4b5891025
Ijaz Ali <62906306+ijaz-ali-dev@users.noreply.github.com>
2022-09-19 09:02:53 -0700

    `declare the variables`

---
+++ Basic Programming/4.js
@@ -0,0 +1,17 @@
+// let keywords
+// declare the variables with let keyword
+
+// var firstname = "iali";
+// firstname = "barball";
+// console.log(firstname); 
+
+// let firstname = "iali";
+// firstname = "barball"; // let ma variable ki value change nhi kr sakta
+// console.log(firstname); 
+
+
+let firstname = "ijazali";
+console.log(firstname); 
+
+// let firstname = "ijaz_ali_is_second_name"; //
+// console.log(firstname);
