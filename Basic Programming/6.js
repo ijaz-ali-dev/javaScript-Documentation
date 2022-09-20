0427335c2b3c1d51098ec7e0c288863d02fb00fe
Ijaz Ali <62906306+ijaz-ali-dev@users.noreply.github.com>
2022-09-20 05:53:13 -0700

    `string indexing function`

---
+++ Basic Programming/6.js
@@ -0,0 +1,22 @@
+// string indexing function
+let first_name = "Lorem ipsum dolor sit amet  consectetur adipisicing elit. Ipsum maxime eius accusantium.";
+
+let second_name = "ali";
+
+// i j a z 
+// 0 1 2 3
+
+// console.log(first_name[0]);
+
+// console.log(first_name.length);
+// console.log(second_name.length);
+// find the last character of the string
+
+// console.log(first_name[first_name.length - 1]);
+
+
+// find the total length of the string
+
+console.log(first_name.length);
+
+// console.log(fist_name + " " + second_name);
