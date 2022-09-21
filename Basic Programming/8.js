71b310f3b1323e11c0a41f9fd61041de197e5687
Ijaz Ali <62906306+ijaz-ali-dev@users.noreply.github.com>
2022-09-21 11:35:09 -0700

    `primitive datatype``string``number``Boolean`undefined``null``bigint & symbol`

--- Basic Programming/8.js
+++ Basic Programming/8.js
@@ -1,1 +1,50 @@
 
+//  typeof operators
+//  ---------------
+// datatype (primitive datatype)
+// 1- string "ijazali"
+// 2- number "12345"
+// 3- booleans "true/false"
+// 4- undefined
+// 5- null
+// 6- bigint (less used)
+// 7- symbol (less used)
+
+
+// 1
+
+let age = 234;
+let fistname = 'John';
+console.log(typeof age);
+console.log(typeof fistname);
+// if we need a simple one character datatype 
+console.log(typeof "o");
+console.log(typeof 234);
+
+
+// convert the number to a string
+let myage = 3424123;
+// console.log(typeof (myage + ""));
+// just add the simple string with number
+let myfirstname = 123456789;
+myfirstname = myfirstname + "";
+console.log(typeof myfirstname);
+
+
+// convert the string to a number
+let mystr = +"23456789";
+console.log(typeof mystr);
+
+
+
+// seocnd method
+
+let ag = 123444; //
+ag = String(ag);
+console.log(typeof ag);
+
+let string = "ijazali iajzali fadfi";
+string = Number(string);
+console.log(typeof string);
+
+
